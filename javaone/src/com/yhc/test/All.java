36d6110e298c368138a11d6a9eb45ac1c9867559
deer2055 <yhc2013198@sina.com>
2017-09-12 13:02:06 +0800

    all

---
+++ javaone/src/com/yhc/test/All.java
@@ -0,0 +1,18 @@
+package com.yhc.test;
+
+public class All implements Ino {
+
+	@Override
+	public void speak() {
+		// TODO Auto-generated method stub
+		System.out.println("这是什么东东");
+
+	}
+
+	@Override
+	public String tell() {
+		// TODO Auto-generated method stub
+		return "这是什么西西!";
+	}
+
+}
