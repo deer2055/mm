935e05b3f00fd18fc92a70cfd073941bf916d33b
deer2055 <yhc2013198@sina.com>
2017-09-12 13:03:09 +0800

    javaone

---
+++ javaone/src/com/yhc/test/Test.java
@@ -0,0 +1,12 @@
+package com.yhc.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         All a = new All();
+         a.speak();
+         System.out.println(a.tell());
+	}
+
+}
